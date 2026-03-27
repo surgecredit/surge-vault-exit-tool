58ee0ed7580882e1f11b8edf581d4f3a0c48e50a
harshavnaik <harsha@blocktheory.com>
2026-03-27 16:07:22 +0530

    public key for testnet updated

--- lib/config.ts
+++ lib/config.ts
@@ -7,7 +7,7 @@ export const APP_CONFIG = {
     unisatNetwork: "testnet",
     networkLabel: "Signet",
     key: "6a1bac977b8af761b330d1473dba1e5cfc75b3256a1ae900b78a369e175423f2",
-    pubkey: "4ea2cf04e6e1823c01e7658f14d822838c1f127451d5ab192460d4fe9fc89fa9",
+    pubkey: "7d258c1ffea13a1231a57cf3c79811c0b51a152f6bb309db75734bbf6cd420b3",
   },
   mainnetConfig: {
     btcApi: "https://mempool.space/api",
